e8915d1597d9dcd6fb7cf284ab163c3695942b7d
StephenZeiler <stephen.zeiler@neudesic.com>
2022-07-07 16:10:08 -0400

    debug

--- API-LocateContractor/Controllers/TestController.cs
+++ API-LocateContractor/Controllers/TestController.cs
@@ -19,7 +19,6 @@ namespace Test_API
             _testContext = context;
         }
 
-        List<Test> list = new List<Test>() { new Test(0, "Stephen"), new Test(1, "Krescens"), new Test(2, "Andrew"), new Test(3, "David"), new Test(4, "Zac") };
 
         [HttpGet]
         public ActionResult<IEnumerable<Test>> Get()

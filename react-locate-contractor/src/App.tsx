af59629ce4cd606a6cba27e3a8631c5ab50e0bfb
StephenZeiler <stephen.zeiler@neudesic.com>
2022-07-10 23:31:14 -0400

    Basic Routing Implementation

--- react-locate-contractor/src/App.tsx
+++ react-locate-contractor/src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import LoginPage from './Pages/LoginPage';
+import HomePage from './Pages/HomePage';
+import BusinessPage from './Pages/BusinessPage';
+import PersonalPage from './Pages/PersonalPage';
 import './App.css';
+import { Link, BrowserRouter as Router, Route, BrowserRouter, Routes, } from 'react-router-dom';
+import styled from 'styled-components';
+import PersonalIcon from './NavBar/PersonalIcon';
+import BusinessIcon from './NavBar/BusinessIcon';
+import HomeIcon from './NavBar/HomeIcon';
+import LoginIcon from './NavBar/LoginIcon';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit s<code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div>
+        <HomeIcon></HomeIcon>
+        <LoginIcon></LoginIcon>
+        <PersonalIcon></PersonalIcon>
+        <BusinessIcon></BusinessIcon>
+      </div>
+      <switch>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/Business" element={<BusinessPage />} />
+          <Route path="/Personal" element={<PersonalPage />} />
+        </Routes>
+      </switch>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ react-locate-contractor/src/NavBar/LoginIcon.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link, BrowserRouter as Router, Route, BrowserRouter, Routes, } from 'react-router-dom';
+import styled from 'styled-components';
+
+function LoginIcon(props) {
+
+    const PrimaryButton = styled.div`
+    position: absolute;
+    width: 106px;
+    height: 34px;
+    left: 1297px;
+    top: 7px;
+    `
+    const ButtonRectangle = styled.div`
+    position: absolute;
+    left: 0px;
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+
+    background: #FFD368;
+    border-radius: 8px;
+    `
+    const ButtonText = styled.div`
+    position: absolute;
+    left: 0px;
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    
+    color: #393E46;
+    `
+    return (
+        <PrimaryButton>
+            <ButtonRectangle>
+                <Link to="/login">
+                    Login
+                </Link>
+            </ButtonRectangle>
+        </PrimaryButton>
+
+    );
+}
+export default LoginIcon;

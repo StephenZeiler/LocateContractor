ee2add0cd6c8bddf78c63ec8423c801de6251222
StephenZeiler <stephen.zeiler@neudesic.com>
2022-07-28 14:06:01 -0400

    update bug fix

--- react-locate-contractor/src/Pages/HomePage.tsx
+++ react-locate-contractor/src/Pages/HomePage.tsx
@@ -1,16 +1,13 @@
 import './SearchBar.css';
 import TextField from '@mui/material/TextField';
 const HomePage = () => {
-    console.log(process.env)
     return (
         <div>
             <h1>Home</h1>
-
             <TextField placeholder='Search for a business...' className="Search-Bar">
                 <p>SEARCH:</p>
                 <input />
             </TextField>
-
         </div>
     );
 }

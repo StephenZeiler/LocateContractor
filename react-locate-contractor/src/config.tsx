71c784409901c8c950f49539e21a480938eb26ba
StephenZeiler <stephen.zeiler@neudesic.com>
2022-07-28 14:07:30 -0400

    update okta URI

--- react-locate-contractor/src/config.tsx
+++ react-locate-contractor/src/config.tsx
@@ -3,12 +3,12 @@ export default {
         issuer: 'https://dev-81535986.okta.com/oauth2/default',
         clientId: '0oa5se2c6gLXwYT2S5d7',
         scopes: ['openid', 'profile', 'email'],
-        redirectUri: `http://localhost:3000/login/callback`
+        redirectUri: `https://yellow-sea-0fa5a340f.1.azurestaticapps.net/login/callback`
     },
     widget: {
         issuer: 'https://dev-81535986.okta.com/oauth2/default',
         clientId: '0oa5se2c6gLXwYT2S5d7',
-        redirectUri: `http://localhost:3000/login/callback`,
+        redirectUri: `https://yellow-sea-0fa5a340f.1.azurestaticapps.net/login/callback`,
         scopes: ['openid', 'profile', 'email'],
         useInteractionCodeFlow: false
     }

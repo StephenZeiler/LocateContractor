8497b2b125ac5fa56c70d2ecdbc8d2e446dcbd32
StephenZeiler <stephen.zeiler@neudesic.com>
2022-08-01 11:04:34 -0400

    merge comments

--- react-locate-contractor/src/config.tsx
+++ react-locate-contractor/src/config.tsx
@@ -3,24 +3,14 @@ export default {
         issuer: 'https://dev-81535986.okta.com/oauth2/default',
         clientId: '0oa5se2c6gLXwYT2S5d7',
         scopes: ['openid', 'profile', 'email'],
-<<<<<<< HEAD
-        redirectUri: `http://localhost:3000/login/callback`
-        // `https://yellow-sea-0fa5a340f.1.azurestaticapps.net/login/callback`
-=======
         redirectUri: `https://yellow-sea-0fa5a340f.1.azurestaticapps.net/login/callback`
         // redirectUri: `http://localhost:3000/login/callback`
->>>>>>> main
     },
     widget: {
         issuer: 'https://dev-81535986.okta.com/oauth2/default',
         clientId: '0oa5se2c6gLXwYT2S5d7',
-<<<<<<< HEAD
-        redirectUri: `http://localhost:3000/login/callback`,
-        // `https://yellow-sea-0fa5a340f.1.azurestaticapps.net/login/callback`,
-=======
         redirectUri: `https://yellow-sea-0fa5a340f.1.azurestaticapps.net/login/callback`,
         // redirectUri: `http://localhost:3000/login/callback`,
->>>>>>> main
         scopes: ['openid', 'profile', 'email'],
         useInteractionCodeFlow: false
     }
